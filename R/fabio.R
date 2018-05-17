1bb0af9236f16256778beac7c26f65640b2da3e3
SLuckeneder <sebastian.luckeneder@wu.ac.at>
2018-05-17 10:44:55 +0200

    add fabio description

---
+++ R/fabio.R
@@ -0,0 +1,13 @@
+#' @title Forestry and Agriculture Biomass Input-Output Tables
+#' @name fabio-package
+#' @aliases fabio
+#' @author Martin Bruckner, \email{martin.bruckner@@wu.ac.at}
+#' 
+#' @description In 2015 we started building physical supply-use and input-output tables covering global agriculture and forestry. 
+#' The work is fully based on freely available data from FAOSTAT, IEA, EIA and UN Comtrade. FABIO now covers 191 countries, 
+#' 116 processes and 130 commodities for the years 1986-2013. The code will be released here in July 2018. An R package is in preparation [1].
+#' 
+#' @references 
+#' [1] ...
+#'  
+NULL
--- R/init.R
+++ R/init.R
@@ -1,4 +1,11 @@
-#' @importFrom utils read.csv
+#' @importFrom utils read.csv packageDescription
 #' @importFrom openxlsx read.xlsx
 #' @importFrom comtradr ct_register_token ct_search
 NULL
+
+.onAttach = function(lib, pkg){
+  packageStartupMessage(
+    sprintf("Loaded fabio v%s. See ?fabio for help, citation(\"fabio\") for use in publication.\n",
+            utils::packageDescription("fabio")$Version) )
+}
+
