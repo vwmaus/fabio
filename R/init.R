95ee4cc68cea549fcc6495ee36b540e949d9ff9f
SLuckeneder <sebastian.luckeneder@wu.ac.at>
2018-05-17 14:11:40 +0200

    add FAO_MRIO_1a_data_preparation()

--- R/init.R
+++ R/init.R
@@ -1,6 +1,8 @@
-#' @importFrom utils read.csv packageDescription
+#' @importFrom utils read.csv read.csv2 packageDescription
 #' @importFrom openxlsx read.xlsx
 #' @importFrom comtradr ct_register_token ct_search
+#' @importFrom data.table data.table as.data.table dcast melt
+#' @importFrom stats aggregate
 NULL
 
 .onAttach = function(lib, pkg){
