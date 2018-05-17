20a41a9cb880e72b38fd6d78a4a00af78e7ab533
SLuckeneder <sebastian.luckeneder@wu.ac.at>
2018-05-17 10:35:04 +0200

    add first function

---
+++ R/FAO_MRIO_0_read_raw_FAO_data.R
@@ -0,0 +1,106 @@
+#' @title FAO_MRIO_0_read_raw_FAO_data
+#' @author Sebastian Luckeneder, \email{sebastian.luckeneder@@wu.ac.at}
+#' 
+#' @description TBA
+#' 
+#' @param ... some argument 
+#' 
+#' @return returns some data
+#' 
+#' @examples 
+#' \dontrun{
+#' 
+#' df <- FAO_MRIO_0_read_raw_FAO_data()
+#' 
+#' }
+#' @export
+FAO_MRIO_0_read_raw_FAO_data <- function(...){
+
+  CBScrop <- utils::read.csv(file="data/raw data/CommodityBalances_Crops_E_All_Data_(Normalized).csv")
+  CBSlvst <- utils::read.csv(file="data/raw data/CommodityBalances_LivestockFish_E_All_Data_(Normalized).csv")
+  CBS_raw <- rbind(CBScrop, CBSlvst)
+  save(CBS_raw, file="data/raw data/CBS_raw.RData")
+  BTD_raw <- utils::read.csv(file="data/raw data/Trade_DetailedTradeMatrix_E_All_Data_(Norm).csv")
+  save(BTD_raw, file="data/raw data/BTD_raw.RData")
+  Forestry_raw <- utils::read.csv(file="data/raw data/Forestry_E_All_Data_(Normalized).csv")
+  save(Forestry_raw, file="data/raw data/Forestry_raw.RData")
+  ForTrade_raw <- utils::read.csv(file="data/raw data/Forestry_Trade_Flows_E_All_Data_(Normalized).csv")
+  save(ForTrade_raw, file="data/raw data/ForTrade_raw.RData")
+  Primary_raw <- utils::read.csv(file="data/raw data/Production_Crops_Primary.csv")
+  save(Primary_raw, file="data/raw data/Primary_raw.RData")
+  Prod_raw <- utils::read.csv(file="data/raw data/Production_Crops_E_All_Data_(Normalized).csv")
+  save(Prod_raw, file="data/raw data/Prod_raw.RData")
+  Proc_lvst_raw <- utils::read.csv(file="data/raw data/Production_LivestockProcessed_E_All_Data_(Normalized).csv")
+  save(Proc_lvst_raw, file="data/raw data/Proc_lvst_raw.RData")
+  Proc_crop_raw <- utils::read.csv(file="data/raw data/Production_CropsProcessed_E_All_Data_(Normalized).csv")
+  save(Proc_crop_raw, file="data/raw data/Proc_crop_raw.RData")
+  Lvst_raw <- utils::read.csv(file="data/raw data/Production_Livestock_E_All_Data_(Normalized).csv")
+  save(Lvst_raw, file="data/raw data/Lvst_raw.RData")
+  LvstPrimary_raw <- utils::read.csv(file="data/raw data/Production_LivestockPrimary_E_All_Data_(Normalized).csv")
+  save(LvstPrimary_raw, file="data/raw data/LvstPrimary_raw.RData")
+  FishProd_raw <- utils::read.csv(file="data/raw data/FishProduction/TS_FI_PRODUCTION.csv")
+  save(FishProd_raw, file="data/raw data/FishProd_raw.RData")
+  ##########################################################################
+  
+  ##########################################################################
+  # Read ethanol data
+  ##########################################################################
+
+    # source: https://www.eia.gov/beta/international/data/browser/#/?pa=000001g&c=ruvvvvvfvtvnvv1urvvvvfvvvvvvfvvvou20evvvvvvvvvnvvuvo&ct=0&tl_id=79-A&vs=INTL.80-1-AFG-TBPD.A&vo=0&v=H&start=1980&end=2014
+  ProdEthanol_EIA <- openxlsx::read.xlsx(xlsxFile="data/raw data/EIA_Biofuels_production.xlsx", rows = 5:235)
+  # source: http://dx.doi.org/10.1787/data-00550-en
+  ProdEthanol_IEA <- openxlsx::read.xlsx(xlsxFile = "data/raw data/IEA_Biogasoline_production.xlsx", startRow = 6)
+  save(ProdEthanol_EIA, ProdEthanol_IEA, file="data/raw data/ProdEthanol_raw.RData")
+  
+  ##########################################################################
+  # Read BACI92 trade data (available from 1995)
+  # source: http://www.cepii.fr/cepii/en/bdd_modele/download.asp?id=1
+  # Attention: restricted access
+  ##########################################################################
+  BACI92 <- utils::read.csv(file=paste0("data/raw data/Baci92/baci92_",1995,".csv"))
+  for(year in 1996:2015){
+    print(year)
+    BACI92 <- rbind(BACI92, utils::read.csv(file=paste0("data/raw data/Baci92/baci92_",year,".csv")))
+    gc()
+  }
+  save(BACI92, file="data/raw data/BACI92_raw.RData")
+  # select Fish (30x) and Ethanol (2207)
+  BACI <- BACI92[substr(BACI92$hs6, 1, 3) %in% c("301","302","303","304") | substr(BACI92$hs6, 1, 4) == "2207", ]
+  save(BACI, file="data/raw data/BACI_selected.RData")
+  ##########################################################################
+  
+  ##########################################################################
+  # Read Comtrade trade data for years prior to 1995 (available from 1988)
+  # Read via API directly from web source
+  # Attention: restricted access
+  # set token to get unrestricted access:
+  token <- "NWXIFLPHElN6pA5nZhwLrpSbph7+cDZ9EsVP9tu62urda5zCBVRH6FU+0/BI/Rq4B6yuz3A71eB2GA3ep5d6VjynVCMotkdYO8wjCiDh47Fls/QflRXygZeb4fonhJ9OP2LZUMAJYo1zwnpiRz3FTbafW8JAIArhG3c0vb/8yxk="
+  # get token: login at https://comtrade.un.org/data/bulk -> reveice token at https://comtrade.un.org/db/u/uAccountInfo.aspx
+  ##########################################################################
+  # install.packages("devtools")
+  # library(devtools)
+  # devtools::install_github("ChrisMuir/comtradr", force = TRUE)
+  
+  comtradr::ct_register_token(token)
+  
+  # load data for HS codes "0301","0302","0303","0304" (Fish) and "2207" (Ethanol)
+  comtrade <- comtradr::ct_search(reporters = "All", 
+                        partners = "All", 
+                        trade_direction = "All", 
+                        start_date = "1986-01-01", 
+                        end_date = "1990-12-31", 
+                        commod_codes = c("0301","0302","0303","0304","2207"))
+  
+  comtrade <- rbind(comtrade, comtradr::ct_search(reporters = "All", 
+                                        partners = "All", 
+                                        trade_direction = "All", 
+                                        start_date = "1991-01-01", 
+                                        end_date = "1994-12-31", 
+                                        commod_codes = c("0301","0302","0303","0304","2207")))
+  
+  save(comtrade, file="data/raw data/comtrade_raw.RData")
+  
+  return(comtrade)
+  
+}
+
---
+++ R/init.R
@@ -0,0 +1,4 @@
+#' @importFrom utils read.csv
+#' @importFrom openxlsx read.xlsx
+#' @importFrom comtradr ct_register_token ct_search
+NULL
